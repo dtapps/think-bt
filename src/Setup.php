e8fd28d0f39d961298c1bda422539a5aa4cc0473
Chaim <gc@dtapp.net>
2020-04-28 15:20:54 +0800

    - update desc

--- src/Setup.php
+++ src/Setup.php
@@ -5,13 +5,13 @@
 // +----------------------------------------------------------------------
 // | 版权所有 2017~2020 [ https://www.dtapp.net ]
 // +----------------------------------------------------------------------
-// | 官方网站: https://gitee.com/liguangchun/bt
+// | 官方网站: https://gitee.com/dtapps/bt
 // +----------------------------------------------------------------------
 // | 开源协议 ( https://mit-license.org )
 // +----------------------------------------------------------------------
-// | gitee 仓库地址 ：https://gitee.com/liguangchun/bt
-// | github 仓库地址 ：https://github.com/GC0202/bt
-// | Packagist 地址 ：https://packagist.org/packages/liguangchun/bt
+// | gitee 仓库地址 ：https://gitee.com/dtapps/bt
+// | github 仓库地址 ：https://github.com/dtapps/bt
+// | Packagist 地址 ：https://packagist.org/packages/liguangchun/think-bt
 // +----------------------------------------------------------------------
 
 namespace DtApp\Think\Bt;
